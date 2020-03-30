9ae82c7b07a00a0f8be7b5dc384c71a2a8a1da7f
agang <2024035028@qq.com>
2020-03-30 09:35:39 +0800

    修改数据库配置

--- app/src/main/java/com/coolweather/android/db/City.java
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -10,6 +10,7 @@ public class City extends LitePalSupport {
     private int id;
     private String cityName;
     private int cityCode;
+    private int provinceId;
 
     public int getId() {
         return id;
@@ -33,5 +34,13 @@ public class City extends LitePalSupport {
 
     public void setCityCode(int cityCode) {
         this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
     }
 }
--- app/src/main/java/com/coolweather/android/db/County.java
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -9,29 +9,6 @@ import org.litepal.crud.LitePalSupport;
 public class County extends LitePalSupport {
     private int id;
     private String countyName;
-    private int countyCode;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getCountyName() {
-        return countyName;
-    }
-
-    public void setCountyName(String countyName) {
-        this.countyName = countyName;
-    }
-
-    public int getCountyCode() {
-        return countyCode;
-    }
-
-    public void setCountyCode(int countyCode) {
-        this.countyCode = countyCode;
-    }
+    private String weatherId;
+    private int cityId;
 }
